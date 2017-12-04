eefb79a0d1acc9dc6d2cc5b9fb80bc34fd6d9c7a
Carlos Antonio Burgos Guevara <carls.burgos@outlook.com>
2017-12-04 15:04:38 -0600

    New version API Net Core Files

---
+++ Models/Prize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public partial class Prize : BaseModel
+    {
+        public string Name { get; set;}
+        
+        public int Stock { get; set;}
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public partial class User: BaseModel
+    {
+        public int First { get; set; }
+
+        public int Last { get; set; }
+    }
+}
