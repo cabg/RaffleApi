510500ad434dd500615b2c254c3e29b7a510c076
Carlos Antonio Burgos Guevara <carls.burgos@outlook.com>
2017-12-04 14:58:11 -0600

    Clean Repository

--- Raffle/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Raffle.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
