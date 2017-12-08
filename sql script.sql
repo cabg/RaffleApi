5c5457295f428467193c16896a8115c29c645944
edwin006 <34038460+edwin006@users.noreply.github.com>
2017-12-08 08:42:43 -0600

    Add files via upload

---
+++ sql script.sql
@@ -0,0 +1,38 @@
+--Clear all data on the following tables
+TRUNCATE TABLE Prizes;
+TRUNCATE TABLE RaffleCounter;
+TRUNCATE TABLE Raffles;
+TRUNCATE TABLE Users;
+
+--Insert the value 1 on the table so it can always star on cicle 1 and counter 1 as well
+INSERT INTO RaffleCounter VALUES (1, 1);
+
+--Insert all the prizes on the table Prizes
+INSERT INTO Prizes VALUES 
+('Pull & Bear 1', 1, 1),
+('Mango', 1, 1), 
+('Sportline', 1, 1),
+('Starbucks', 8, 1),
+('Forever', 5, 1),
+('Bennigans', 1, 1),
+('Opi', 2, 1),
+('Pampa', 2, 1),
+('Ruby Tuesday', 1, 1),
+('Coffee Cup', 2, 1),
+('Fly Wings', 2, 1),
+('Sushi ito', 2, 1),
+('Pizza Hut', 8, 1),
+('Dennis', 3, 1),
+('Wallmart', 5, 1),
+('Super Selectos', 2, 1),
+('Siman', 2, 1),
+('Movie Tickets', 25, 1),
+('Breshka', 2, 1),
+('Pull & Bear 2', 2, 1),
+('Zara', 2, 1),
+('Stradivarius', 2, 1),
+('Sento', 2, 1);
+
+
+
+
